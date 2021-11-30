1d7b9aecb5a011eeee1fcdcea323ea8d0469f13e
ohuji <juhsalomaki@gmail.com>
2021-11-30 14:16:56 +0200

    Aloitus aktiviteetin logiikkaa ja muiden aktiviteettien tiedostot

--- app/src/main/java/project/rt_running_tracker/MainActivity.java
+++ app/src/main/java/project/rt_running_tracker/MainActivity.java
@@ -2,7 +2,9 @@ package project.rt_running_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onStartClick(View v) {
+        Intent intent = new Intent(this, ExerciseActivity.class);
+        startActivity(intent);
+    }
 
+    public void onHistoryClick(View v) {
+        Intent intent = new Intent(this, HistoryActivity.class);
+        startActivity(intent);
+    }
+
+    public void onProfileClick(View v) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
 }
