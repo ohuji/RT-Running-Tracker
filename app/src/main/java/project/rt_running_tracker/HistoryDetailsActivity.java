298172b3aae2e66edc14857b3e86d2859068630e
jannhakk <Janne.Hakkarainen@metropolia.fi>
2021-12-14 19:29:23 +0200

    Lisätty javadoc kommentteja

--- app/src/main/java/project/rt_running_tracker/HistoryDetailsActivity.java
+++ app/src/main/java/project/rt_running_tracker/HistoryDetailsActivity.java
@@ -1,4 +1,9 @@
 package project.rt_running_tracker;
+/** HistoryDetails aktiviteetti
+ * @Janne Hakkarainen
+ * @Juho Salomäki
+ * Remy Silanto
+ */
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,16 +22,27 @@ public class HistoryDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_details);
 
+        /**
+         * Intentin avulla saadaan edellisessä aktiviteetissa luotu indeki.
+         * Luodaan muuttuja datei ja indeksiä hyödyntäen saadaan haettua oikea pvm+indeksi
+         */
         Bundle b = getIntent().getExtras();
         int i = b.getInt(HistoryActivity.EXTRA);
         String datei = HistoryData.getInstance().gethistoryData(i).toString();
 
+        /**
+         * Jos kuvan indeksi on alle kymmenen käytetään alla olevaa if lausetta jolla saadaan oikea indeksi kuvalle.
+         * Jos indeksi on yli kymmenen tai enemmän saadaan oikea kuva haettua muuttujalla datei
+         */
         if (i < 10) {
             int j = Integer.parseInt(datei.substring(0, 1));
             String a = datei.substring(1);
             j++;
             String s = j + a;
 
+            /**
+             * Kuvakenttään asetaan oikeaa juoksua vastaava kuva
+             */
             //Kuvakenttään asetaan oikeaa juoksua vastaava kuva
             File mapImage = new File("/data/data/project.rt_running_tracker/files/image" + s + ".png");
 
@@ -51,6 +67,9 @@ public class HistoryDetailsActivity extends AppCompatActivity {
             }
         }
 
+        /**
+         * Tekstikenttään asetetaan indeksiä vastaavan juoksun tiedot
+         */
         //Tekstikenttään asetetaan indeksiä vastaavan juoksun tiedot
         ((TextView)findViewById(R.id.tvSavedSteps))
                 .setText(HistoryData.getInstance().gethistoryData(i).getStep());
--- app/src/main/java/project/rt_running_tracker/ProfileData.java
+++ app/src/main/java/project/rt_running_tracker/ProfileData.java
@@ -1,5 +1,11 @@
 package project.rt_running_tracker;
 
+/** Profiilin luonti
+ * @Janne Hakkarainen
+ * @Juho Salomäki
+ * Remy Silanto
+ */
+
 public class ProfileData {
 
     private String userName;
@@ -7,27 +13,51 @@ public class ProfileData {
     private int userHeight;
     private String userGender;
 
+    /**
+     * Luo profiilin jossa on kyseiset parametrit
+     * @param userName käyttäjän profiilinimi
+     * @param userWeight käyttäjän paino
+     * @param userHeight käyttäjän pituus
+     * @param userGender käyttäjän sukupuoli
+     */
     public ProfileData(String userName, int userWeight, int userHeight, String userGender) {
 
         this.userName = userName;
         this.userWeight = userWeight;
         this.userHeight = userHeight;
         this.userGender = userGender;
+
     }
 
+    /**
+     * Haetaan käyttäjän nimi
+     * @return stringinä käyttäjän nimen
+     */
     //Luotu metodit joilla saadaan haettua käyttäjän syöttämät tiedot
     public String getUserName(){
         return this.userName;
     }
 
+    /**
+     * Haetaan käyttäjän paino
+     * @return int käyttäjän paino
+     */
     public int getUserWeight(){
         return this.userWeight;
     }
 
+    /**
+     * Haetaan käyttäjän pituus
+     * @return int käyttäjän pituus
+     */
     public int getUserHeight(){
         return this.userHeight;
     }
 
+    /**
+     * Haetaan käyttäjän sukupuoli
+     * @return stringinä käyttäjän sukupuoli
+     */
     public String getUserGender(){
         return this.userGender;
     }
